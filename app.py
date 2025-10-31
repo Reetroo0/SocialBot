4196894a09d74129307fd1329032ed1556403f93
Arch <ArchKDE@example.com>
2025-10-31 22:08:12 +0300

    Теперь бот работает быстрее и при выключении пишет сообщение что выключен

--- app.py
+++ app.py
@@ -1,28 +1,35 @@
-import asyncio
-from config import bot, dp ,logger
+from aiogram import run_polling
+from config import bot, dp, logger
 from misc.pgSQL import pgConnect
 from misc.keyboards import main_menu
 from handlers import start, survey, UnCompSurvey, profile, menu
 
+# Подключаем роутеры
+dp.include_routers(
+    start.router,
+    survey.router,
+    UnCompSurvey.router,
+    profile.router,
+    menu.router
+)
 
-async def main():
-    # Подключение хендлеров
-    dp.include_routers(start.router)
-    dp.include_routers(survey.router)
-    dp.include_routers(UnCompSurvey.router)
-    dp.include_routers(profile.router)
-    dp.include_routers(menu.router)
-    
-    # Подключение к БД
+async def on_startup():
     pgConnect()
+    await bot.send_message(618425933, 'Бот запущен', reply_markup=main_menu)
+    logger.info("Бот запущен с workers!")
 
-    # Уведомление запуске бота
-    await bot.send_message(chat_id=618425933, text='Бот запущен', reply_markup=main_menu)
-
-    # Запуск бота
-    await dp.start_polling(bot)
+async def on_shutdown():
+    await bot.send_message(618425933, 'Бот остановлен')
+    await bot.session.close()
+    logger.info("Бот остановлен — ресурсы освобождены")
 
 
 if __name__ == '__main__':
-    logger.info("Бот запущен")
-    asyncio.run(main())
+    run_polling(
+        bot,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        workers=4,
+        polling_timeout=10,
+        allowed_updates=dp.resolve_used_update_types()
+    )
